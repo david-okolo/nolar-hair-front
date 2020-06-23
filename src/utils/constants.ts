cf47bc278552031871fd6b75fced42e1ac56c8bb
David Okolo <david.okolo@icloud.com>
2020-06-23 17:25:20 +0100

    Install dependencies and create helpers

---
+++ src/utils/constants.ts
@@ -0,0 +1,30 @@
+export const backendUrl = 'https://nolarhair.com.ng/api';
+export const backendStaticUrl = 'https://nolarhair.com.ng'
+
+export const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json'
+};
+
+export const links = [
+  {
+    name: 'about',
+    to: '/about'
+  },
+  {
+    name: 'services',
+    to: '/services'
+  },
+  {
+    name: 'testimonals',
+    to: '/testimonials'
+  },
+  {
+    name: 'store',
+    to: '/store'
+  },
+  {
+    name: 'check booking',
+    to: '/booking'
+  }
+]
